cb92510d6eaa3a923cff2133fb02be6eca29e53b
Raghav-rv28 <raghavrudhra28@gmail.com>
2020-12-20 12:47:11 -0500

    Added Assets to Island Rodeo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{ 
+    private CharacterController2D cc= new CharacterController2D();
+    public TextMeshProUGUI Cointext;
+	public TextMeshProUGUI Timetext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cointext.text="Total Score:"+ cc.getScore();
+        Timetext.text="Total Time Taken:"+Mathf.RoundToInt(cc.getTime()).ToString();
+    }
+
+    public void triggerMenuBehaviour(int i) {
+        switch(i){
+            default:
+            case(0):
+            SceneManager.LoadScene("Level1");
+            break;
+            case(1):
+            Application.Quit();
+            break;
+        }
+    }
+}
